3e3c1548a465e36139056c4ffd8dc87705c7d084
Windows313 <57021443+Windows313@users.noreply.github.com>
2020-11-09 11:46:52 +0200

    Add files via upload

---
+++ love.sh
@@ -0,0 +1,104 @@
+#
+#lebanon_cyber_Team
+#
+
+pkg install toilet -y
+pkg install nano -y
+gem install lolcat
+clear
+toilet -f  mono12 -F gay "hello"
+toilet -f pagga "lyom badna nkamel jeze2 tene mn l 2osaa" | lolcat
+
+toilet -f pagga "ma la7 tkon tawele le2ano heye 3al bash" | lolcat
+
+toilet -f pagga "ken ya maken" | lolcat
+toilet -f pagga "b2adem l zamen" | lolcat
+toilet -f pagga "t3araf chab 3a banut 7elwe kterr" | lolcat
+
+toilet -f pagga "w t3ala3 feha kterrrr" | lolcat
+
+toilet -f pagga "w 2asar eno ykamel ma3a" | lolcat
+
+toilet -f pagga "bas hay l benet ma ken bada tkamel ma3o" | lolcat
+
+toilet -f pagga "le2ano majroha mn chab 2ablo" | lolcat
+
+toilet -f pagga "2asar l chab 3layha" | lolcat
+
+toilet -f pagga "w dall kell yom ye7keha" | lolcat
+
+toilet -f pagga "w yehtam feha" | lolcat
+
+toilet -f  mono12 -F gay "w hon balshet l 2ossa"
+
+toilet -f pagga "lama 2alet l banut lal chab" | lolcat
+
+toilet -f pagga "bet7ebne..? " | lolcat
+
+toilet -f pagga "hon l chab 3eref ena balachet t7eselo be che" | lolcat
+
+toilet -f pagga "w sar yehtam feha 2aktar w 2aktarr" | lolcat
+
+toilet -f pagga "w kell y5anela" | lolcat
+
+toilet -f pagga "w y2ella bn8 2a el ma tnem" | lolcat
+
+toilet -f pagga "w y2ella bnjur l sobe7" | lolcat
+
+toilet -f pagga "la 7ad ma 2eja yom" | lolcat
+
+toilet -f pagga "w 2alet l banut lal chab" | lolcat
+toilet -f  mono12 -F gay "i love you"
+
+toilet -f pagga "w mara2 se3a w tnen" | lolcat
+
+toilet -f pagga "w ma sem3et jaweb" | lolcat
+
+toilet -f pagga "na7et l messag w ba3atet hello" | lolcat
+
+toilet -f pagga "kmn ma hada rad" | lolcat
+
+toilet -f pagga "hon l benet 3asabet" | lolcat
+
+toilet -f pagga "w 3emletlo block" | lolcat
+
+toilet -f pagga "w tene nhar" | lolcat
+
+toilet -f pagga "saret tes2al ref2ato" | lolcat
+
+toilet -f pagga "2alet ma hada 3am yred" | lolcat
+
+toilet -f pagga "w ma 3ad 7at story " | lolcat
+
+toilet -f pagga "hon l benet 5afet kter" | lolcat
+
+toilet -f pagga "w cheletlo l block" | lolcat
+
+toilet -f pagga "sar teb3atlo alo alo alo" | lolcat
+
+toilet -f pagga "w ma hada yred" | lolcat
+
+toilet -f pagga "T2akadet eno fe che 8alat" | lolcat
+
+toilet -f pagga "ma saket talephone baya" | lolcat
+
+toilet -f pagga "da2etloo..! " | lolcat
+toilet -f pagga "rad 3layha cha5es kberr" | lolcat
+toilet -f pagga "w 2aletlo bade 2e7le l chab" | lolcat
+toilet -f pagga "w hon rad 3layha be sot 7azen kterr" | lolcat
+toilet -f  mono12 -F gay "allah yer7amo"
+toilet -f pagga "ken m3asab w kn 3a yso2" | lolcat
+
+toilet -f pagga "w nchallah l 3omer 2elkon" | lolcat
+
+toilet -f pagga "hek betkon 5olset l 2ossa" | lolcat
+
+toilet -f pagga "w bel neheye kelna la7 nmot" | lolcat
+
+toilet -f pagga "bas ma had bya3ref 2emata " | lolcat
+
+toilet -f pagga "fa lezelek 7ebo ba3ed" | lolcat
+toilet -f pagga "kono 2ed wehde" | lolcat
+toilet -f pagga "ma teksro b5ater hada" | lolcat
+toilet -f pagga "nchallah tkon 3ajbtkon l 2ossa" | lolcat
+toilet -f pagga "byee..!" | lolcat
